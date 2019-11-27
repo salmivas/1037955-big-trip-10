e4bb8a44dc650ea9d43ac8a8f4e67e87a39d9ba4
Ivan Salmin <salmin.iv@gmail.com>
2019-11-27 20:02:37 +0400

    Divides the project on modules. Adjusts project building using Webpack.

---
+++ src/components/route-element.js
@@ -0,0 +1,7 @@
+export const createRouteElement = () => {
+  return `<div class="trip-info__main">
+  <h1 class="trip-info__title">Amsterdam — ... — Amsterdam</h1>
+
+  <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;21</p>
+</div>`;
+};
