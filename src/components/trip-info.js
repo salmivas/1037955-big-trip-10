352240747fdc32f4cbf40cfa1f204882671a469d
Ivan Salmin <salmin.iv@gmail.com>
2019-12-17 23:22:48 +0400

    Creates rough sketch of mocks

---
+++ src/components/trip-info.js
@@ -0,0 +1,9 @@
+export const createTripInfo = () => {
+  return (
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam — ... — Amsterdam</h1>
+
+      <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;21</p>
+    </div>`
+  );
+};
--- src/main.js
+++ src/main.js
@@ -1,23 +1,60 @@
-import {createRouteElement} from './components/route-element';
-import {createSiteMenuTemplate} from './components/site-menu';
-import {createFilterTemplate} from './components/filter';
-import {createTripSort} from './components/trip-sort';
-import {createEventItem} from './components/event';
-import {createTripList} from './components/trip-list';
+import {
+  createTripInfo
+} from './components/trip-info';
+import {
+  createTripInfoCost
+} from './components/trip-cost.js';
+import {
+  createTripTabs
+} from './components/trip-tabs';
+import {
+  createTripFilters
+} from './components/trip-filter.js';
+import {
+  createTripSort
+} from './components/trip-sort.js';
+import {
+  createEventItem
+} from './components/create-event.js';
+import {
+  createTripList,
+} from './components/trip-list.js';
+import {
+  generateEvent,
+  createDates,
+} from './mock/event.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const tripInfo = document.querySelector(`.trip-info`);
-render(tripInfo, createRouteElement(), `afterbegin`);
+render(tripInfo, createTripInfo(), `afterbegin`);
 
-const tripControlsSwitchElement = document.querySelector(`.trip-controls h2:first-child`);
-render(tripControlsSwitchElement, createSiteMenuTemplate(), `afterend`);
+const tripTabs = document.querySelector(`.trip-controls h2:first-child`);
+render(tripTabs, createTripTabs(), `afterend`);
 
 const tripControlsFilterElement = document.querySelector(`.trip-controls h2:last-child`);
-render(tripControlsFilterElement, createFilterTemplate(), `afterend`);
+render(tripControlsFilterElement, createTripFilters(), `afterend`);
 
 const tripEventsElement = document.querySelector(`.trip-events`);
+const createEvent = generateEvent();
+render(tripEventsElement, createTripSort(), `beforeend`);
+render(tripEventsElement, createEventItem(createEvent), `beforeend`);
 
-[createTripSort, createEventItem, createTripList].forEach((it) => render(tripEventsElement, it(), `beforeend`));
+const dates = createDates(3);
+
+dates.forEach((date, i) => {
+  date.dayNumber = ++i;
+  render(tripEventsElement, createTripList(date), `beforeend`);
+});
+
+const sumTripCost = dates
+.slice()
+.reduce((sum, current) => {
+  return sum + current.events.reduce((evtSum, evtCur) => {
+    return evtSum + evtCur.cost;
+  }, 0);
+}, 0);
+
+render(tripInfo, createTripInfoCost(sumTripCost), `beforeend`);
